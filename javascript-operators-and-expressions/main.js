ee9744ddf0c46c7e80ba47b642e9e1a81b2e26ca
Holly Phung <hollyphung95@gmail.com>
2022-12-14 17:56:20 +0000

    completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,38 @@
+var height = 10;
+var width = 5;
+
+var resultVal = height * width;
+console.log('value of area: ', resultVal);
+console.log('typeof area: ', typeof resultVal);
+
+var bill = 10;
+var payment = 20;
+var change = payment - bill;
+
+console.log('value of change: ', change);
+console.log('typeof change: ', typeof change);
+
+var quizzes = 90;
+var midterm = 85;
+var final = 75;
+
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade: ', grade);
+console.log('typeof grade: ', typeof grade);
+
+var firstName = 'Holly';
+var lastName = 'Phung';
+
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullNAme: ', fullName);
+console.log('typeof area: ', typeof fullName);
+
+var pH = 3;
+var isAcidic = pH < 7;
+console.log('value of area: ', isAcidic);
+console.log('typeof area: ', typeof isAcidic);
+
+var motto = fullName;
+motto = fullName + ' is the GOAT';
+console.log('value of area: ', motto);
+console.log('typeof area: ', typeof motto);
