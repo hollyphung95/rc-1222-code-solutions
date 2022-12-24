ea117250738ccd0cdd803f5640e2ab751ba8414a
Holly Phung <hollyphung95@gmail.com>
2022-12-24 12:42:52 +0000

    come back to string index swap.. the rest is complete.

---
+++ string-manipulation-2/src/swap-chars.js
@@ -0,0 +1,24 @@
+/* exported swapChars */
+
+/* Parameters
+firstIndex - an integral JavaScript Number
+secondIndex - an integral JavaScript Number
+string - any JavaScript String
+Return Value  - string with the characters at firstIndex and secondIndex swapped.  */
+
+/* function swapChars(firstIndex, secondIndex, string) {
+  var strArray = [];
+  var resultValue = '';
+
+  for (var i = 0; i < string.length; i++) {
+    strArray.push(string.charAt(i));
+  }
+
+  var buffer1 = string.charAt(firstIndex);
+
+  //string.charAt(firstIndex) = string.charAt(secondIndex);
+  //string.charAt(secondIndex) = buffer1;
+  resultValue = strArray.toString();
+
+  return resultValue;
+} */
