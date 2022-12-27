2623dc20d2b838aae043caa6c2161f54a7ba5954
Holly Phung <hollyphung95@gmail.com>
2022-12-27 19:23:48 +0000

    saving previous opened page. incomplete.

---
+++ object-manipulation-2/src/invert.js
@@ -0,0 +1,19 @@
+/* /* exported invert */
+/* Creates an object composed of the inverted keys and values of source.
+
+Parameters
+source - Any JavaScript Object
+Return Value
+A new Object containing all properties of source, but with the keys and values inverted.
+*/
+/*
+function invert(source) {
+  var returnNewObj = {};
+
+  for (var i = 0; i < source.length - 1; i++) {
+    var tempVariable;
+
+  }
+
+}
+ */
