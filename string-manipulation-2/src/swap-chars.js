53ff32ee65c251725300bc432f1165b51fdf6ede
Holly Phung <hollyphung95@gmail.com>
2022-12-30 05:07:38 +0000

    complete string-manipulation-2

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -6,19 +6,18 @@ secondIndex - an integral JavaScript Number
 string - any JavaScript String
 Return Value  - string with the characters at firstIndex and secondIndex swapped.  */
 
-/* function swapChars(firstIndex, secondIndex, string) {
-  var strArray = [];
+function swapChars(firstIndex, secondIndex, string) {
   var resultValue = '';
 
   for (var i = 0; i < string.length; i++) {
-    strArray.push(string.charAt(i));
+    if (i === firstIndex) {
+      resultValue += string[secondIndex];
+    } else if (i === secondIndex) {
+      resultValue += string[firstIndex];
+    } else {
+      resultValue += string[i];
+    }
+
   }
-
-  var buffer1 = string.charAt(firstIndex);
-
-  //string.charAt(firstIndex) = string.charAt(secondIndex);
-  //string.charAt(secondIndex) = buffer1;
-  resultValue = strArray.toString();
-
   return resultValue;
-} */
+}
