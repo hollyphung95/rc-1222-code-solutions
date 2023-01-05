8f67b5de42a5fa41804e7fa6506bac6c73d02a50
Holly Phung <hollyphung95@gmail.com>
2023-01-05 16:15:15 +0000

    tests for Account section that still fail include deposit/ withdraw positive amount and getbalance if transactions is not empty.

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -1,1 +1,40 @@
 /* exported Account */
+
+class Account {
+
+  constructor(number, holder) {
+    this.name = 'Account';
+    this.number = number;
+    this.holder = holder;
+    this.transactions = [];
+    this.deposits = [];
+    this.withdraws = [];
+  }
+
+  getBalance() {
+    if (this.transactions.length === 0) {
+      return 0;
+    }
+    return this.amount;
+  }
+
+  deposit(amount) {
+    var returnValue = parseInt(this.amount);
+
+    if (returnValue > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  withdraw(amount) {
+    if (this.amount > 0) {
+      this.transactions.push(['withdraw', amount]);
+      return this.Transaction('withdraw', this.amount);
+    } else {
+      return false;
+    }
+  }
+
+}
