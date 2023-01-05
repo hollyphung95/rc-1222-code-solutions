af2b63c0e0fb3dff98a96c9f50876abf2746dca3
Holly Phung <hollyphung95@gmail.com>
2023-01-05 04:44:42 +0000

    Completed javascript-constructors exercise

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,38 @@
+/* In the provided new-objects.js,
+complete the following tasks for each of these built-in JavaScript constructors:
+Object, Array, Function, String, Number, and Boolean: */
+
+/* Call the constructor with new and assign the return value to a variable.
+e.g. var anObject = new Object(); */
+
+/* Log the value of the instance to the console.
+e.g. console.log('value of anObject:', anObject); */
+
+/* Log the typeof the instance to the console.
+e.g. console.log('typeof anObject:', typeof anObject); */
+
+/* repeat for Array, Function, String, Number, and Boolean. */
+
+var myObj = new Object();
+console.log('value of myObj :', myObj);
+console.log('type of myObj :', typeof myObj);
+
+var myArr = new Array();
+console.log('value of myArr :', myArr);
+console.log('type of myArr :', typeof myArr);
+
+var myFunc = new Function();
+console.log('value of myFunc:', myFunc);
+console.log('type of myFunc:', typeof myFunc);
+
+var myStr = new String();
+console.log('value of myStr :', myStr);
+console.log('type of myStr :', typeof myStr);
+
+var myNum = new Number();
+console.log('value of myNum :', myNum);
+console.log('type of myNum :', typeof myNum);
+
+var myBool = new Boolean();
+console.log('value of myBool :', myBool);
+console.log('type of myBool :', typeof myBool);
